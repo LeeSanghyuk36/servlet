edd6b6bb4b9a0f6c52eb25fac8bfade25d68b5dd
unknown <makada23@gmail.com>
2024-07-18 16:12:24 +0900

    servlet lecture 20

--- workspace/JSPPrj/src/main/java/com/newlecture/web/Nana.java
+++ workspace/JSPPrj/src/main/java/com/newlecture/web/Nana.java
@@ -9,15 +9,26 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/hello")
+@WebServlet("/hi")
 public class Nana extends HttpServlet{
 	@Override
 	protected void service(HttpServletRequest req, 
-			HttpServletResponse res) 
-			throws ServletException, IOException {
-		
+							HttpServletResponse res) 
+			throws ServletException, IOException 
+	{
+		res.setCharacterEncoding("UTF-8");
+		res.setContentType("text/html; charset=UTF-8");
 		PrintWriter out = res.getWriter();
-		out.println("Hello ~~~");
+		
+		String cnt_ = req.getParameter("cnt");
+		int cnt = 3;
+		if (cnt_ != null && !cnt_.equals("")) {
+			cnt = Integer.parseInt(cnt_);			
+		}
+		for(int i =0; i<cnt;i++)
+		{
+			out.println((i+1)+": こんにちは Servlet! <br >");
+		}
 	}
 
 }
