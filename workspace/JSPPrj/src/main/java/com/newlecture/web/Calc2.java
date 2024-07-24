41594b4971c545bccce3418617bba137bc2b2e0a
unknown <makada23@gmail.com>
2024-07-24 17:53:13 +0900

    servlet 26 v2

--- workspace/JSPPrj/src/main/java/com/newlecture/web/Calc2.java
+++ workspace/JSPPrj/src/main/java/com/newlecture/web/Calc2.java
@@ -3,6 +3,7 @@ package com.newlecture.web;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -21,30 +22,32 @@ public class Calc2 extends HttpServlet{
 		res.setContentType("text/html; charset=UTF-8");
 		PrintWriter out = res.getWriter();
 		
-		String x_ = req.getParameter("x");
-		String y_ = req.getParameter("y");
+		String v_ = req.getParameter("v");
 		String op = req.getParameter("operator");
-		int x = 0, y = 0;
-		if (x_ != null && y_ != null && !x_.equals("") && !y_.equals("")) {
-			x = Integer.parseInt(x_);
-			y = Integer.parseInt(y_);
-//			System.out.printf("result is");
-//			out.println("plus ");
-//			out.println(x+y );
-//			out.println("<br > minus ");
-//			out.println(x-y);
-//			out.println("<br > multiply ");
-//			out.println(x*y);
-//			out.println("<br > divine");
-//			out.println(x/y);
+		int v = 0;
+		if (v_ != null && !v_.equals("") ) {
+			v = Integer.parseInt(v_);
 		}
-		int result = 0;
-		if (op.equals("plus")) {
-			result = x+y;			
-		}else if (op.equals("minus")) {
-			result = x-y;			
-		}
-		out.printf("result is %d\n", result);
+		
+		// calc
+//		if (op.equals("=")) {
+//			int result = 0;
+//			if (op.equals("plus")) {
+//				result = x+y;			
+//			}else if (op.equals("minus")) {
+//				result = x-y;			
+//			}
+//			
+//			//save value
+//		}else {
+//			ServletContext application = req.getServletContext();
+//			application.setAttribute("value", v);
+//			application.setAttribute("op", op);
+//		
+//			
+//		}
+//		
+//		out.printf("result is %d\n", result);
 	}
 
 }
